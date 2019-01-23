e2803ff58030d29d53e775da2043564394dfb632
kezhenxu94 <kezhenxu94@163.com>
2019-01-23 20:13:33 +0800

    add parameterized methods

--- dubbo-basic-api/src/main/java/org/apache/dubbo/demo/api/DemoService.java
+++ dubbo-basic-api/src/main/java/org/apache/dubbo/demo/api/DemoService.java
@@ -2,11 +2,17 @@ package org.apache.dubbo.demo.api;
 
 
 import org.apache.dubbo.demo.model.Result;
+import org.apache.dubbo.demo.model.User;
 
 /**
  * @author zmx ON 2018/10/30
  */
 public interface DemoService {
+    Result sayHello();
 
     Result sayHello(String name);
+
+    Result sayHello(Long id, String name);
+
+    Result sayHello(User user);
 }
--- dubbo-basic-provider/src/main/java/org/apache/dubbo/demo/impl/DemoServiceImpl.java
+++ dubbo-basic-provider/src/main/java/org/apache/dubbo/demo/impl/DemoServiceImpl.java
@@ -6,6 +6,7 @@ import org.apache.dubbo.common.logger.Logger;
 import org.apache.dubbo.common.logger.LoggerFactory;
 import org.apache.dubbo.demo.api.DemoService;
 import org.apache.dubbo.demo.model.Result;
+import org.apache.dubbo.demo.model.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -21,9 +22,28 @@ public class DemoServiceImpl implements DemoService {
     private String name;
 
     @Override
+    public Result sayHello() {
+        return new Result(name, "Hello from default");
+    }
+
+    @Override
     public Result sayHello(String localName) {
         logger.info("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext
                 .getContext().getRemoteAddress());
         return new Result(name, "Hello " + localName + ", response from provider: " + RpcContext.getContext().getLocalAddress());
+    }
+
+    @Override
+    public Result sayHello(final Long id, final String name) {
+        return sayHello(new User(id, name));
+    }
+
+    @Override
+    public Result sayHello(final User user) {
+        String localName = user.getUsername();
+        Long id = user.getId();
+        logger.info("[" + new SimpleDateFormat("HH:mm:ss").format(new Date()) + "] Hello " + name + ", request from consumer: " + RpcContext
+            .getContext().getRemoteAddress());
+        return new Result(name, "Hello " + id + " " + localName + ", response from provider: " + RpcContext.getContext().getLocalAddress());
     }
 }
